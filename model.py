67189776b49de93cff488094acd8be46352c5824
adriana railean <adriana_railean@hotmail.com>
2021-05-29 13:39:27 +0200

    Added model.py

---
+++ model.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+'''
+You need to structure your implementation in a way that in this class the prediction
+of the different engagement types of your model are called. Please replace the 
+random return placeholder with you actual implementation.
+
+You can train different models for each engagement type or you train one which is able
+to predicte multiple classes.
+'''
+
+def reply_pred_model(input_features):
+    # TODO fill in your implementation of the model
+    return np.random.rand()
+
+def retweet_pred_model(input_features):
+    # TODO fill in your implementation of the model
+    return np.random.rand()
+
+def quote_pred_model(input_features):
+    # TODO fill in your implementation of the model
+    return np.random.rand()
+
+def fav_pred_model(input_features):
+    # TODO fill in your implementation of the model
+    return np.random.rand()
